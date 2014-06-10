15e53d687533b716843ac7a2a2c95b8bf2cd2467
FunkyMonkeyMonk <monkey@buildingbananas.com>
2014-06-09 20:46:37 -0400

    Clean up code
    Cleaning up code and creating a run command for later use as a go routine.

--- firestarter.go
+++ firestarter.go
@@ -7,13 +7,56 @@ import (
   "strings"
 )
 
+func logfail(err error) {
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func run(client *docker.Client, options docker.CreateContainerOptions, host_config docker.HostConfig) {
+  //Create the container
+  container, err := client.CreateContainer(options)
+  logfail(err)
+
+  var container_id = container.ID
+
+  // Start the container
+  err = client.StartContainer(container_id, &host_config)
+  logfail(err)
+
+  // Wait for the container to finish
+  _, err = client.WaitContainer(container_id)
+  logfail(err)
+
+  // Create a stream to read from docker container
+  var buf bytes.Buffer
+
+  // Read the log from the container
+  err = client.AttachToContainer(docker.AttachToContainerOptions{
+    Container:    container_id,
+    OutputStream: &buf,
+    Logs:         true,
+    Stdout:       true,
+    Stderr:       true,
+  })
+  logfail(err)
+
+  // Print the output to the terminal
+  log.Println(buf.String())
+
+  // Remove the container
+  err = client.RemoveContainer(docker.RemoveContainerOptions{
+    ID: container_id,
+    Force: true,
+  })
+  logfail(err)
+}
+
 func main() {
   // Attach to local docker instance
   endpoint := "unix:///docker.sock"
   client, err := docker.NewClient(endpoint)
-  if err != nil {
-    log.Fatal(err)
-  }
+  logfail(err)
 
   // Define command in string array format
   cmd := "echo\n'Hello World'"
@@ -32,56 +75,5 @@ func main() {
   // Define the container config
   host_config := docker.HostConfig{}
 
-  // ------------------------------------------------------------------
-  // This section will eventually be it's own goroutine
-  // ------------------------------------------------------------------
-
-  //Create the container
-  container, err := client.CreateContainer(options)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  var container_id = container.ID
-
-  // Start the container
-  err = client.StartContainer(container_id, &host_config)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  // Wait for the container to finish
-  _, err = client.WaitContainer(container_id)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  // Create a stream to read from docker container
-  var buf bytes.Buffer
-
-  // Read the log from the container
-  err = client.AttachToContainer(docker.AttachToContainerOptions{
-    Container:    container_id,
-    OutputStream: &buf,
-    Logs:         true,
-    Stdout:       true,
-    Stderr:       true,
-  })
-  
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  // Print the output to the terminal
-  log.Println(buf.String())
-
-  // Remove the container
-  err = client.RemoveContainer(docker.RemoveContainerOptions{
-    ID: container_id,
-    Force: true,
-  })
-  
-  if err != nil {
-    log.Fatal(err)
-  }
+  run(client, options, host_config)
 }
